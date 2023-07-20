d23414a688bf1019ca0ca920e5027474d450e700
byMagg <byMag@hotmail.es>
2023-07-20 14:47:40 +0200

    reducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,27 +1,39 @@
 import useCounter from "../hooks/useCounter";
 import Button from "../components/Button"
 import Panel from "../components/Panel"
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+    return {
+        ...state,
+        count: state.count + 1
+    };
+};
 
 function CounterPage({ initialCount }) {
-    const { count, setCount, handleClick } = useCounter(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0);
+    const { setCount, handleClick } = useCounter(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0);
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    });
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
-        setValueToAdd(value);
+        // setValueToAdd(value);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        setCount(prev => prev + valueToAdd);
-        setValueToAdd(0);
+        // setCount(prev => prev + valueToAdd);
+        // setValueToAdd(0);
     }
 
     return (
         <Panel className="m-3">
-            <h1 className="text-lg">Count is {count}</h1>
+            <h1 className="text-lg">Count is {state.count}</h1>
             <div className="flex flex-row">
                 <Button onClick={handleClick}>
                     Increment
@@ -31,7 +43,7 @@ function CounterPage({ initialCount }) {
             <form onSubmit={handleSubmit}>
                 <label>Add</label>
                 <input
-                    value={valueToAdd || ''}
+                    value={state.valueToAdd || ''}
                     onChange={handleChange}
                     type="number"
                     className="p-1 m-3 bg-gray-50 border border-gray-300" />
